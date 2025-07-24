ed253d9e1bb497c869e62ac7d8b2f5f257a9c378
Brault sebastien <braseb37@gmail.com>
2025-07-24 23:23:55 +0200

    Implement a getCurrentUser fonction

--- src/main/java/com/paymybuddy/app/service/CustomUserDetailsService.java
+++ src/main/java/com/paymybuddy/app/service/CustomUserDetailsService.java
@@ -3,6 +3,8 @@ package com.paymybuddy.app.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,22 +21,28 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 				
-		Optional<User> userOpt = userService.getUserByEmail(email);
-		if (userOpt.isPresent()) {
-			User user = userOpt.get();
-			return org.springframework.security.core.userdetails.User.builder()
-                    .username(user.getUsername())
-                    .password(user.getPassword())
-                    .build();
-			//return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+		User user = userService.getUserByEmail(email)
+								.orElseThrow(() ->  new UsernameNotFoundException(String.format("User with email {%s} not found", email)));
+					
+		return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .build();
+		//return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
 		}
-		
-		else {
-			throw new UsernameNotFoundException(String.format("User with email {%s} not found", email));
-		}
-		
-		
-		
+	
+	public User getCurrentUser() {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+	    if (auth != null && auth.isAuthenticated()) {
+	        String email = auth.getName(); // ou getPrincipal().getUsername()
+	        System.out.println(email);
+	        System.out.println(auth);
+	        return userService.getUserByEmail(email)
+	                .orElseThrow(() -> new RuntimeException("Utilisateur introuvable en base"));
+	    }
+
+	    throw new RuntimeException("Utilisateur non connecté");
 	}
 	
 }
